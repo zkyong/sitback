b2cc7603106f0a0dbe8be54ca9865af52a86fb6a
zkyong <254261163@qq.com>
2018-02-06 11:24:50 +0800

    修正com.zkyong.test.excel.ExcelTest注释(乱码)

--- util-test-web/README.md
+++ util-test-web/README.md
@@ -1,2 +1,4 @@
 # util-test-web
 To build a util-test poject-web.
+2018年2月6日 11:23:02<br/>
+<pre>    修正com.zkyong.test.excel.ExcelTest注释(乱码).</pre><br/>
--- util-test-web/src/com/zkyong/test/excel/ExcelTest.java
+++ util-test-web/src/com/zkyong/test/excel/ExcelTest.java
@@ -15,15 +15,15 @@ import com.zkyong.exception.excel.ExcelException;
 import com.zkyong.util.excel.ExcelUitl;
 
 /**
- * ExcelUtil��excel���������������<BR>
+ * ExcelUtil Excel处理工具<BR>
  * @author zkyong<BR>
- * @date 2017��9��28�� 15:34:16<BR>
+ * @date 2018年2月6日 11:05:54<BR>
  * @version 1.0.0<BR>
  */
 public class ExcelTest {
 
     public static void main(String[] args) {
-        String fullPath = "C:/Users/zkyong/Desktop/ѧ���ɼ���.xls " ; //�����excel�ļ���   
+        String fullPath = "C:/Users/zkyong/Desktop/测试表格.xls " ; 
         listToExcelTest(fullPath);
         System.out.println(","+excelToListTest(fullPath));
         //String str = "college";
@@ -31,8 +31,8 @@ public class ExcelTest {
     }
     
     /**
-     * �������Excel��ָ��·��<BR>
-     * @param fullPath ȫ·��<BR>
+     * 将对象list转换为Excel表格<BR>
+     * @param fullPath 全路径<BR>
      */
     private static void listToExcelTest(String fullPath) {
         Student student = null;
@@ -40,35 +40,35 @@ public class ExcelTest {
         for (int i = 0; i < 198 ; i++) {
             student = new Student();
             student.setId("ID000"+(i+1));
-            student.setName("ѧ��"+(i+1));
+            student.setName("姓名"+(i+1));
             student.setSex(new BigDecimal(Math.random()).setScale(0, BigDecimal.ROUND_HALF_UP).intValue());
             student.setScore(new BigDecimal(Math.random()*50).intValue()+50);
             student.setAge(new BigDecimal(Math.random()*3).intValue() + 15);
             College college = new College();
             college.setCollegeId(new BigDecimal(Math.random()*5).intValue());
-            college.setCollegeName("ѧԺ"+college.getCollegeId());
+            college.setCollegeName("学院名称"+college.getCollegeId());
             School school = new School();
             school.setSchoolId("SL2010100");
-            school.setSchoolName("XXXX��ѧ");
+            school.setSchoolName("学校名称");
             college.setSchool(school);
             student.setCollege(college);
             list.add(student);
             System.out.println(student);
         }
         LinkedHashMap<String, String> fieldMap = new LinkedHashMap<>();;
-        fieldMap.put("id", "ѧ��");
-        fieldMap.put("name", "����");
-        fieldMap.put("sex", "�Ա�");
-        fieldMap.put("age", "����");
-        fieldMap.put("score", "����");
-        fieldMap.put("college.collegeId", "ѧԺ���");
-        fieldMap.put("college.collegeName", "ѧԺ����");
-        fieldMap.put("college.school.schoolId", "ѧУ���");
-        fieldMap.put("college.school.schoolName", "ѧУ����");
+        fieldMap.put("id", "学生ID");
+        fieldMap.put("name", "姓名");
+        fieldMap.put("sex", "性别");
+        fieldMap.put("age", "年龄");
+        fieldMap.put("score", "分数");
+        fieldMap.put("college.collegeId", "学院ID");
+        fieldMap.put("college.collegeName", "学院名称");
+        fieldMap.put("college.school.schoolId", "学校ID");
+        fieldMap.put("college.school.schoolName", "学校名称");
         OutputStream os = null;
         try {
             os = new  FileOutputStream(fullPath);
-            ExcelUitl.listToExcel(list, fieldMap, "ѧ���ɼ���", 200, os);
+            ExcelUitl.listToExcel(list, fieldMap, "学生表", 200, os);
         } catch (ExcelException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
@@ -85,26 +85,27 @@ public class ExcelTest {
     }
     
     /**
-     * ���Դ�Excel��ȡ��List<T><BR>
+     * 将Excel表格转换为对象list<T><BR>
      * @param fullPath ȫ·��<BR>
      */
     private static List<Student> excelToListTest(String fullPath) {
         InputStream in = null;
         List<Student> list = null;
         LinkedHashMap<String, String> fieldMap = new LinkedHashMap<>();
-        fieldMap.put("ѧ��", "id");
-        fieldMap.put("����", "name");
-        fieldMap.put("�Ա�", "sex");
-        fieldMap.put("����", "age");
-        fieldMap.put("����", "score");
-        fieldMap.put("ѧԺ���", "college.collegeId");
-        fieldMap.put("ѧԺ����", "college.collegeName");
-        fieldMap.put("ѧУ���","college.school.schoolId");
-        fieldMap.put("ѧУ����","college.school.schoolName");
-        String[] uniqueFields = {"ѧ��"};
+        fieldMap.put("学生ID", "id");
+        fieldMap.put("姓名", "name");
+        fieldMap.put("性别", "sex");
+        fieldMap.put("年龄", "age");
+        fieldMap.put("分数", "score");
+        fieldMap.put("学院ID", "college.collegeId");
+        fieldMap.put("学院名称", "college.collegeName");
+        fieldMap.put("学校ID","college.school.schoolId");
+        fieldMap.put("学校名称","college.school.schoolName");
+        
+        String[] uniqueFields = {"学生ID"};
         try {
             in = new FileInputStream(fullPath);
-            list = ExcelUitl.excelToList(in,"ѧ���ɼ���1",Student.class,fieldMap,uniqueFields);
+            list = ExcelUitl.excelToList(in,"学生表1",Student.class,fieldMap,uniqueFields);
         } catch (ExcelException e1) {
             e1.printStackTrace();
         } catch (FileNotFoundException e) {
